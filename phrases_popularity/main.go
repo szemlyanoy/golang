98c6d4629a42c0e7eddab80cc32101bdbb2f5cdf
Sergey Zemlyanoy <szemlyanoy@Sergeys-MacBook-Pro.local>
2021-04-04 15:11:57 -0400

    fan-in/fan-out channles for per-chunk processing

--- phrases_popularity/main.go
+++ phrases_popularity/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -11,16 +12,14 @@ import (
 	"sort"
 	"strings"
 	"sync"
-	"bytes"
 )
-
 
 type occurence struct {
 	Phrase string
 	Count  int
 }
+
 var mu sync.Mutex
-var wg sync.WaitGroup
 var prgrphOccrncsUniqueMerged []occurence
 
 type ByCount []occurence
@@ -34,20 +33,19 @@ func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("Filename must be passed")
 		return
-	    }
+	}
+	chanPopulated := make(chan string) // to push paragraphs into
+	chanProcessed := make(chan occurence) // to push paragraphs into
+
 	var buff bytes.Buffer
 	var data string
 	var err error
-	for i:=1; i<len(os.Args); i++{
+	for i := 1; i < len(os.Args); i++ {
 		file, _ := ioutil.ReadFile(os.Args[i])
 		buff.WriteString(string(file))
-		}
+	}
 	data = buff.String()
 	errCheck(err)
-	reg, err := regexp.Compile(`[^\w\n\s]+`)
-	errCheck(err)
-	dataRaw := reg.ReplaceAllString(data, "") // strip non-alpanumeric chars
-	dataRaw = strings.ToLower(dataRaw)
 
 	files, err := filepath.Glob("*.json")
 	errCheck(err)
@@ -56,84 +54,35 @@ func main() {
 		errCheck(err)
 	}
 
-	//  take substring, split by paragraph
-	prgrphs := strings.Split(dataRaw, "\n")
-	for i := range prgrphs {
-		wg.Add(1)
-		go do_prgrph(prgrphs[i], i)
-		fmt.Println("Goroutines:", runtime.NumGoroutine())
-		//fmt.Println("Goroutines:", runtime.NumGoroutine())   // to print goroutines #
-	}
-	wg.Wait()
+	// ##########################################
+	go populate(data, chanPopulated)
+	go process(chanPopulated, chanProcessed)
 
-	fmt.Println("goroutines completed, starting aggregation")
-
-	// aggregation. Range over prgrphOccrncsUniqueMerged and find matching struct and aggregate counts
-	var prgrphOccrncsUniqueAggr []occurence
-	for _, v := range prgrphOccrncsUniqueMerged {
+        // aggregation. Range over prgrphOccrncsUniqueMerged and find matching struct and aggregate counts
+        var prgrphOccrncsUniqueAggr []occurence
+	for v := range chanProcessed{
 		skip := false
-		for i, u := range prgrphOccrncsUniqueAggr {
-			if v.Phrase == u.Phrase {
-				prgrphOccrncsUniqueAggr[i].Count += v.Count
-				skip = true
-			}
-		}
-		if !skip {
-			prgrphOccrncsUniqueAggr = append(prgrphOccrncsUniqueAggr, v)
-		}
-		//		  fmt.Println(prgrphOccrncsUniqueAggr)
+                for i, u := range prgrphOccrncsUniqueAggr {
+                        if v.Phrase == u.Phrase {
+                                prgrphOccrncsUniqueAggr[i].Count += v.Count // sum up Counts
+                                skip = true
+                        }
+                }
+                if !skip {
+                        prgrphOccrncsUniqueAggr = append(prgrphOccrncsUniqueAggr, v)
+                }
 	}
 
 	sort.Sort(ByCount(prgrphOccrncsUniqueAggr))
+
 	// dump first 100 structs sorted in descending way into json
-	outputLen := cap(prgrphOccrncsUniqueAggr)
-	if outputLen > 100{ outputLen = 100}
+	outputLen := len(prgrphOccrncsUniqueAggr)
+	if outputLen > 100 {outputLen = 100}
+
 	file, _ := json.MarshalIndent(prgrphOccrncsUniqueAggr[:outputLen], "", " ")
 	_ = ioutil.WriteFile("output.json", file, 0644)
 
 	fmt.Printf("\n== Results stored in output.json ===\n")
-
-}
-
-func do_prgrph(prgrph string, indx int) {
-	var prgrphOccrncs []occurence
-	var prgrphOccrncsUnique []occurence
-	
-	words := strings.Fields(prgrph)
-	for j := 2; j < len(words); j++ { // starting on 2 to catch 3-words phrase from beginning
-		substr := occurence{
-			Phrase: strings.Join([]string{words[j-2], words[j-1], words[j]}, " "),
-			Count:  0,
-		}
-		//phraseMatches := strings.Count(prgrph, lookupPhrase)
-		lookupPhraseRegexp := regexp.MustCompile(`(?i)` + substr.Phrase)
-		phraseMatches := lookupPhraseRegexp.FindAllStringIndex(prgrph, -1)
-		substr.Count = len(phraseMatches)
-		prgrphOccrncs = append(prgrphOccrncs, substr)
-	}
-	
-	// now we might have duplicate structs after processing paragraph
-	for _, v := range prgrphOccrncs {
-		skip := false
-		for _, u := range prgrphOccrncsUnique {
-			if v == u {
-				skip = true
-				break
-			}
-		}
-		if !skip {
-			mu.Lock()
-			prgrphOccrncsUnique = append(prgrphOccrncsUnique, v)
-			mu.Unlock()
-		}
-	}
-
-	// push to global variable accessible for goruotines
-	for _,v := range prgrphOccrncsUnique{
-		prgrphOccrncsUniqueMerged = append(prgrphOccrncsUniqueMerged, v)
-	}
-	fmt.Printf("paragraph %v complete\n", indx)
-	wg.Done()
 }
 
 // ==============
@@ -142,5 +91,83 @@ func errCheck(e error) {
 		panic(e)
 	}
 }
+
 // ==============
 
+//  take substring, split by paragraph
+func populate(d string, c chan<- string) {
+	prgrphs := strings.Split(d, "\n\n")
+
+	for i := range prgrphs {
+		prgrph := prgrphs[i]
+		if len(prgrph) == 0 {
+			continue
+		} // to skip empty elements
+		c <- prgrph
+	}
+	close(c)
+}
+
+// to return channel of prgrphOccrncsUniqueMerged
+func process(cIn <-chan string, cOut chan<- occurence) {
+	var wg sync.WaitGroup
+	for p := range cIn {
+		if len(p) == 0 {
+			continue
+		} // to skip empty elements
+		wg.Add(1)
+		go func(prgrph string) {
+			fmt.Println("Goroutines:", runtime.NumGoroutine())
+
+			var prgrphOccrncs []occurence
+			var prgrphOccrncsUnique []occurence
+
+			// replcing characters and extra spaces
+			reg, err := regexp.Compile(`[^\w+]`)
+		        errCheck(err)
+		        prgrph = reg.ReplaceAllString(prgrph, " ")
+                        reg, err = regexp.Compile(`\s{2,}`)
+                        errCheck(err)
+                        prgrph = reg.ReplaceAllString(prgrph, " ")
+			prgrph = strings.ToLower(prgrph)
+
+			words := strings.Fields(prgrph) // to construct searching phrases
+
+			for j := 2; j < len(words); j++ { // starting on 2 to catch 3-words phrase from beginning
+				substr := occurence{
+					Phrase: strings.Join([]string{words[j-2], words[j-1], words[j]}, " "),
+					Count:  0,
+				}
+				lookupPhraseRegexp := regexp.MustCompile(substr.Phrase)
+				phraseMatches := lookupPhraseRegexp.FindAllStringIndex(prgrph, -1)
+				substr.Count = len(phraseMatches)
+				prgrphOccrncs = append(prgrphOccrncs, substr)
+			}
+
+			// now we might have duplicate structs after processing paragraph
+			for _, v := range prgrphOccrncs {
+				skip := false
+				for _, u := range prgrphOccrncsUnique {
+					if v == u {
+						skip = true
+						break
+					}
+				}
+				if !skip {
+					prgrphOccrncsUnique = append(prgrphOccrncsUnique, v)
+				}
+			}
+
+			// push to channle, fan-in
+			for _, v := range prgrphOccrncsUnique {
+				cOut <- v
+			}
+
+			fmt.Printf("paragraph complete\n")
+			wg.Done()
+		}(p)
+	}
+
+	wg.Wait()
+	close(cOut)
+}
